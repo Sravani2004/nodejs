e3c73a15f5e23d87ea9b42cf9a3f17567fafd181
Sravani <92629103+Sravani2004@users.noreply.github.com>
2024-05-02 11:47:29 +0530

    Delete invas directory

--- invas/src/Login.jsx
+++
@@ -1,7 +0,0 @@
-//Login.jsx
-import React from "react";
-
-function Login() {
-  return <div>Login</div>;
-}
-export default Login;
--- invas/src/Register.jsx
+++
@@ -1,7 +0,0 @@
-//Register.jsx
-import React from "react";
-
-function Register() {
-  return <div>Register</div>;
-}
-export default Register;
